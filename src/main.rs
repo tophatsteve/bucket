aef901350e1478881e23d3e220acb8c54a0d8fa2
Steve Bentley <steve@codebazaar.co.uk>
2018-09-18 13:41:40 +0100

    Add coverage to travis build

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,22 @@ extern crate notify;
 extern crate sentry;
 
 use failure::err_msg;
-use notify::{watcher, RecursiveMode, Watcher};
+use notify::{watcher, DebouncedEvent, RecursiveMode, Watcher};
 use sentry::integrations::failure::capture_error;
 use sentry::integrations::panic::register_panic_handler;
-use std::sync::mpsc::channel;
+use std::sync::mpsc::{channel, Receiver};
 use std::time::Duration;
+
+struct Config {
+    root_folder: String,
+}
 
 fn main() {
     // uses SENTRY_DSN to create connection
     let _guard = sentry::init(());
     register_panic_handler();
+
+    let config = get_default_config();
 
     // Create a channel to receive the events.
     let (tx, rx) = channel();
@@ -24,7 +30,7 @@ fn main() {
     // Add a path to be watched. All files and directories at that path and
     // below will be monitored for changes.
 
-    match watcher.watch("/bucket", RecursiveMode::Recursive) {
+    match watcher.watch(&config.root_folder, RecursiveMode::Recursive) {
         Ok(_) => (),
         Err(e) => {
             capture_error(&err_msg(e.to_string()));
@@ -32,6 +38,10 @@ fn main() {
         }
     }
 
+    event_loop(rx);
+}
+
+fn event_loop(rx: Receiver<DebouncedEvent>) {
     loop {
         match rx.recv() {
             Ok(event) => println!("{:?}", event),
@@ -40,5 +50,11 @@ fn main() {
                 println!("watch error: {:?}", e);
             }
         }
+    }
+}
+
+fn get_default_config() -> Config {
+    Config {
+        root_folder: String::from("/bucket"),
     }
 }
